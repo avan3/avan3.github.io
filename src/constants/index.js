11c967265002f9303b519d51a8b53ac1767daf70
Andrew <andrew.van1@live.com>
2024-04-15 18:08:42 -0400

    update menu to use icons and custom hamburger menu

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,14 +1,26 @@
+import { FaPersonCircleQuestion } from "react-icons/fa6";
+import { RiListCheck3, RiContactsBook2Line } from "react-icons/ri";
+import { MdOutlineWorkOutline } from "react-icons/md";
+
 export const navLinks = [
   {
     id: "about",
     title: "About",
+    icon: FaPersonCircleQuestion,
   },
   {
     id: "skills",
     title: "Skills",
+    icon: RiListCheck3,
   },
   {
     id: "work",
     title: "Work",
+    icon: MdOutlineWorkOutline,
+  },
+  {
+    id: "contact",
+    title: "Contact",
+    icon: RiContactsBook2Line,
   },
 ];
