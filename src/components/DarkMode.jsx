fde87bbcb1a24de7072ae474c6c9666b73207fca
Andrew <andrew.van1@live.com>
2024-04-17 17:05:29 -0400

    updated darkmode switch appearance on mobile and desktop and moved theme property to parent navbar

--- src/components/DarkMode.jsx
+++ src/components/DarkMode.jsx
@@ -1,65 +1,31 @@
-import React, { useState, useEffect } from "react";
-import Button from "./Button";
-import { GiMoon } from "react-icons/gi";
-import { BiSolidSun } from "react-icons/bi";
+import React from "react";
 
-const DarkMode = () => {
-  const [theme, setTheme] = useState(
-    localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
-  );
-
-  // access to html element
-  const element = document.documentElement;
-
-  // set theme to localStorage and html element
-  useEffect(() => {
-    localStorage.setItem("theme", theme);
-    if (theme === "dark") {
-      element.classList.add("dark");
-    } else {
-      element.classList.remove("dark");
-    }
-  });
-
-  // TODO: can enhance with more themes
-  // useEffect(() => {
-  //   const selectedTheme = localStorage.getItem("theme");
-
-  //   if (selectedTheme) {
-  //     console.log("selectedTheme: ", selectedTheme);
-  //     document.documentElement.classList.add(selectedTheme);
-  //   } else if (window.matchMedia("(prefers-color-sceme: dark").matches) {
-  //     document.documentElement.classList.add("dark");
-  //   } else {
-  //     document.documentElement.classList.add("light");
-  //   }
-  // }, []);
-
+const DarkMode = ({ theme, setTheme }) => {
   return (
-    <>
-      <button
-        type="btn"
-        className="text-secondary relative w-[24px] h-[24px] m-2"
-        onClick={() => setTheme(theme == "dark" ? "light" : "dark")}
-      >
-        <BiSolidSun
-          className={`w-[24px] h-[24px] duration-300 absolute top-0 ${
-            theme === "dark" ? "opacity-100" : "opacity-0"
-          }`}
-        />
-        <GiMoon
-          className={`w-[24px] h-[24px] duration-300 absolute top-0 ${
-            theme === "dark" ? "opacity-0" : "opacity-100"
-          }`}
-        />
-      </button>
-      {/* <Button
-        text="dark"
-        handler={() => setTheme(theme == "dark" ? "light" : "dark")}
-        bgColor="bg-primary"
-        textColor="text-white"
-      /> */}
-    </>
+    <button type="btn" className="relative w-[24px] h-[24px]">
+      <input
+        onChange={() =>
+          setTheme((prev) => (prev === "dark" ? "light" : "dark"))
+        }
+        className="mr-2 mt-[0.3rem] h-3.5 w-8 appearance-none rounded-[0.4375rem] bg-orange-400
+          before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full 
+          before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] 
+          after:h-5 after:w-5 after:rounded-full after:border-none after:bg-primary
+          after:shadow-[0_0px_3px_0_rgb(0_0_0_/_7%),_0_2px_2px_0_rgb(0_0_0_/_4%)] 
+          after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] 
+          checked:bg-orange-400 checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] 
+          checked:after:ml-[1.0625rem] checked:after:h-5 checked:after:w-5 checked:after:rounded-full 
+          checked:after:border-none checked:after:bg-primary 
+          checked:after:shadow-[0_3px_1px_-2px_rgba(0,0,0,0.2),_0_2px_2px_0_rgba(0,0,0,0.14),_0_1px_5px_0_rgba(0,0,0,0.12)] 
+          checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] 
+          hover:cursor-pointer dark:bg-orange-400 dark:after:bg-primary dark:checked:bg-orange-400 
+          dark:checked:after:bg-primary"
+        type="checkbox"
+        role="switch"
+        id="flexSwitchCheckDefault"
+        checked={theme === "dark"}
+      />
+    </button>
   );
 };
 
