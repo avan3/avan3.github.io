6e4b4c7907419a522dcd4b30a8249eb6209f79ab
Andrew <andrew.van1@live.com>
2024-04-18 15:58:43 -0400

    add tailwind-merge to project for more reusable buttons

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,5 @@
 import React, { ButtonHTMLAttributes, ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
@@ -8,8 +9,10 @@ const Button = ({ children, className, ...props }) => {
   return (
     <button
       {...props}
-      className={`${className}
-        font-semibold cursor-pointer hover:scale-105 duration-300 py-2 px-8 rounded-full relative z-10`}
+      className={twMerge(
+        "font-semibold cursor-pointer hover:scale-105 duration-300 py-2 px-8 rounded-full relative z-10",
+        className
+      )}
     >
       {children}
     </button>
