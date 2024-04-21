c05d14ede0a603dcf7d9dd326ce11899e7661e61
Andrew <andrew.van1@live.com>
2024-04-21 16:08:12 -0400

    updated the about me section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,14 +7,27 @@ const About = () => {
       id="about"
       className="bg-orange-600 text-white p-10 flex justify-center"
     >
-      <section className="w-full sm:w-[640px] text-center">
+      <section className="w-full sm:w-[720px] text-center">
         <h2 className={`${styles.heading2}`}>About Me.</h2>
         <hr className="w-48 h-1 mx-auto my-4 bg-white border-0 rounded md:my-5 \" />
-        <p className={`font-noto_sans font-normal ${styles.paragraph}`}>
-          Hey! I&apos;m Andrew Van. A software developer from Toronto, Ontario
-          with professional experience developing high quality web applications.
-          <br />
-          Looking to bring your next project to life.
+        <p
+          className={`font-noto_sans font-normal ${styles.paragraph} text-left`}
+        >
+          Hey! I&apos;m Andrew Van. A seasoned software developer dedicated to
+          crafting exceptional web experiences.
+        </p>
+        <p
+          className={`font-noto_sans font-normal ${styles.paragraph} text-left my-2`}
+        >
+          With a strong foundation in web development technologies and a passion
+          for problem-solving, I thrive on turning ideas into polished digital
+          solutions that perform flawlessly across devices.
+        </p>
+        <p
+          className={`font-noto_sans font-normal ${styles.paragraph} text-left`}
+        >
+          Let&apos;s collaborate to elevate your online presence and create
+          something truly remarkable together
         </p>
       </section>
     </section>
